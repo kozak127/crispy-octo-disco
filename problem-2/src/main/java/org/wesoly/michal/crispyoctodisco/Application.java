e9328493568dbde0c5450fa9e86536e037c3ac52
mwesoly <michal.wesoly@nitrobox.de>
2020-10-10 04:09:22 +0200

    problem-2 Added InputReader with tests

--- problem-2/src/main/java/org/wesoly/michal/crispyoctodisco/Application.java
+++ problem-2/src/main/java/org/wesoly/michal/crispyoctodisco/Application.java
@@ -1,12 +1,18 @@
 package org.wesoly.michal.crispyoctodisco;
 
+import java.io.IOException;
+import java.util.List;
+
 public class Application {
 
-    public static void main(String[] args) {
-        System.out.println(new Application().getProject());
-    }
+    public static void main(String[] args) throws IOException {
+        InputReader inputReader = new InputReader();
 
-    public String getProject() {
-        return "problem-2";
+        List<String> expressions = inputReader.read();
+
+        ExpressionEvaluator evaluator = new ExpressionEvaluator();
+
+        List<String> evaluatedExpressions = evaluator.evaluate(expressions);
+        System.out.println(evaluatedExpressions);
     }
 }
---
+++ problem-2/src/main/java/org/wesoly/michal/crispyoctodisco/ExpressionEvaluator.java
@@ -0,0 +1,11 @@
+package org.wesoly.michal.crispyoctodisco;
+
+import java.util.List;
+import org.apache.commons.lang3.NotImplementedException;
+
+public class ExpressionEvaluator {
+
+    public List<String> evaluate(List<String> expressions) {
+        throw new NotImplementedException();
+    }
+}
